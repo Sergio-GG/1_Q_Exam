f12bc8516d3d07bffd31fec93c715bdcb839fa94
Sergio <sergio-guitar@yandex.ru>
2023-03-25 13:00:52 +0300

    added task

---
+++ TASK/Program.cs
@@ -0,0 +1,68 @@
+﻿// Написать программу, которая из имеющегося массива слов
+// формирует массив из строк, длина которых меньше либо равна 3 символам.
+// Первоначальный массив можно ввести с клавиатуры, либо задать в начале.
+// Использовать массивы.
+
+
+
+
+string[] CreateArray(int size)
+{
+    string[] array = new string[size];
+    for (int i = 0; i < size; i++)
+    {   
+        Console.WriteLine("Введите слово");
+        string word = Console.ReadLine();
+        array[i] = word;
+    }
+    return array;
+}
+
+int Counter (string[] array)
+{
+    int counter = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3) counter++;
+    }
+    return counter;
+}
+
+string[] ArrayOfPassedWords(string[] array, int newSize)
+{
+    int index = 0;
+    string[] newArray = new string[newSize];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3) 
+        {
+            newArray[index] = array[i];
+            index++;
+        }
+    }
+    return newArray;
+}
+    
+void PrintStringArray(string[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1)
+        Console.Write($"\"{array[i]}\", ");
+        else Console.Write($"\"{array[i]}\"");
+    }
+    Console.WriteLine("]");
+}
+
+Console.WriteLine("Введите желаемое количество слов");
+int size = Convert.ToInt32(Console.ReadLine());
+string[] arrayOfWords = CreateArray(size);
+
+PrintStringArray(arrayOfWords);
+
+int counter = Counter(arrayOfWords);
+Console.WriteLine($"Количество слов до 4 знаков --> {counter}");
+string[] arrayPassed = ArrayOfPassedWords(arrayOfWords, counter);
+
+PrintStringArray(arrayPassed);
